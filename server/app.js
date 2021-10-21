85f834c06e95076b55bc202855bf2d7c827eeed1
Arthur Baker <artbaker82@gmail.com>
2021-10-20 21:31:05 -0700

    added server folder

---
+++ server/app.js
@@ -0,0 +1,21 @@
+//todo
+//set up express router
+//set up database
+//create user schema
+//set up authentication endpoints
+import express from "express";
+import mongoose from "mongoose";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+//connect to database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

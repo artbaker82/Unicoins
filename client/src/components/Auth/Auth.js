f094aaf6cf71240984545504baa2ad54f0f27d12
dales <dalesimmons0210@gmail.com>
2021-11-06 21:42:08 -0700

    basic login frontend setup

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -32,12 +32,30 @@ const Auth = () => {
 
     //just an email and a password to mock login to an account
     return (
-        <div>
+        
+        <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
+            <h3 className="text-2xl font-bold text-center">Login to your account</h3>
             <form onSubmit={handleSubmit}>
-                <input name='email' label='Email Address' handleChange={handleChange} type='email'/>
-                <input name='password' label='Password' handleChange={handleChange} type='password'/>
+                <div className="mt-4">
+                    <div>
+                        <label class="block" for="email">Email</label>
+                                <input name='email' label='Email Address' handleChange={handleChange} type='email'
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                />
+                    </div>
+                    <div class="mt-4">
+                        <label className="block">Password</label>
+                                <input type="password" placeholder="Password" handleChange={handleChange}
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+                    </div>
+                    <div className="flex items-baseline justify-between">
+                        <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</button>
+                        
+                    </div>
+                </div>
             </form>
         </div>
+    
     )
 }
 
